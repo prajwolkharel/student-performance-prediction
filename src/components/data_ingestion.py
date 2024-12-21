f83fdfef4481db58cd4df22d3d6e611f0c2c1b11
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-21 19:52:49 +0545

    Implemented Model Training Scripts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTainer
 
 
 @dataclass
@@ -52,4 +53,7 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
