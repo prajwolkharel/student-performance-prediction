57514f1bc6b55879b63624867f36f717cfaabf65
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-21 16:11:37 +0545

    Implemented Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -49,3 +50,6 @@ class DataIngestion():
 if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
